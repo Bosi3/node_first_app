34ef177d4cf7efd2790bc024f7093328d4be7a6a
hang <35296190+Bosi3@users.noreply.github.com>
2018-12-07 16:26:40 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
-// import the NPM dependancy package
-const express     = require("express");
+const express = require("express");
+
 const bodyParser  = require("body-parser");
+const app     = express();
 
-// initialise express() inside of your app variable
-const app = express();
-
-// parse body of incoming json requests
-app.use(bodyParser.json());
-
-// import route modules and pass your app
-require("./routes/demoRoutes")(app);
 require("./routes/userRoutes")(app);
 
-// choose what port on which to run the server
-const PORT = 5000;
+const PORT    = 5000;
 
-// use the app variable and listen on the port
 app.listen(PORT, () => {
   console.log(`Server running`);
 });
+app.use(bodyParser.json());
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,101 +1,35 @@
 const jsonfile = require("jsonfile");
-const file_path = "./DB/users.json";
-
 module.exports = app => {
+var file = '/tmp/data.json'
+jsonfile.readFile(file, function(err, obj) {
+  console.dir(obj)
+})
+app.get("/users", (req, res) => {
+  console.log("fetching all users");
 
-  app.put("/user", (req, res) => {
-
-    let user;
-    let username = req.body.username;
-    let email    = req.query.email;
-
-    jsonfile.readFile(file_path, function(err, content) {
-      for (var i = content.length - 1; i >= 0; i--) {
-        if (content[i].email === req.query.email) {
-
-          console.log("updated user " + req.query.email + " has now username : " + username);
-
-          user = content[i];
-          user.username = username;
-
-        }
-      }
-
-      jsonfile.writeFile(file_path, content, function(err) {
-        console.log(err);
-      });
-
-    });
-    res.send(user);
+  // jsonfile reading
+  jsonfile.readFile("./DB/users.json", function(err, content) {
+    // send file contents back to sender
+    res.send(content);
   });
+});
+app.post("/users/new", (req, res) => {
+  console.log("fetching all users");
+  
+  let username = req.body.username;
+  let email    = req.query.email;
+  var obj = {name: 'JP'}
+  jsonfile.readFile("./DB/users.json", function(err, content) {
 
-  app.delete("/users/destroy", (req, res) => {
+    content.push({ email: email, username: username });
 
-    let email = req.body.email;
+    console.log("added " + email + "to DB");
 
-    jsonfile.readFile(file_path, function(err, content) {
-
-      for (var i = content.length - 1; i >= 0; i--) {
-
-        if (content[i].email === email) {
-          console.log("removing " + content[i].email + "from DB");
-          content.pop(i);
-        }
-
-      }
-
-      jsonfile.writeFile(file_path, content, function(err) {
-        console.log(err);
-      });
-
-      res.sendStatus(200);
+    jsonfile.writeFile("./DB/users.json", content, function(err) {
+      console.log(err);
     });
+
+    res.sendStatus(200);
   });
-
-  app.get("/users", (req, res) => {
-    console.log("fetching all users");
-
-    jsonfile.readFile(file_path, function(err, content) {
-      res.send(content);
-    });
-  });
-
-  app.get("/user", (req, res) => {
-
-    let user;
-    let username = req.query.username;
-
-    jsonfile.readFile(file_path, function(err, content) {
-
-      for (var i = content.length - 1; i >= 0; i--) {
-
-        if (content[i].username === username) {
-          console.log("user found");
-          console.log(content[i]);
-          user = content[i];
-        }
-
-      }
-
-      res.send(user);
-    });
-  });
-
-  app.post("/users/new", (req, res) => {
-
-    let { email, username } = req.body;
-
-    jsonfile.readFile(file_path, function(err, content) {
-
-      content.push({ email, username });
-
-      console.log("added " + email + "to DB");
-
-      jsonfile.writeFile(file_path, content, function(err) {
-        console.log(err);
-      });
-
-      res.sendStatus(200);
-    });
-  });
-};
+});
+} 

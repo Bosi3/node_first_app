921cd99f60264859a4a33fe84d15a84b479b9b1b
hang <35296190+Bosi3@users.noreply.github.com>
2018-12-10 15:32:21 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const bodyParser  = require("body-parser");
 const app     = express();
-
+app.use(bodyParser.json());
 require("./routes/userRoutes")(app);
 
 const PORT    = 5000;
@@ -10,4 +10,3 @@ const PORT    = 5000;
 app.listen(PORT, () => {
   console.log(`Server running`);
 });
-app.use(bodyParser.json());
